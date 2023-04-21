4df9277936b70f905faceb2fc5c6eea0933472a1
orionpgaete <pedro.gaete@pgcomputer.cl>
2023-04-21 12:09:41 -0400

    incial

---
+++ proyecto_23/templatesApp/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def rendertemplate(request):
+    data = {"nombre": "Pedro"}
+    return render(request, 'template/plantilla.html', data)
+
+def renderinfo(request):
+    data = {"id": "123", "nombre": "Pedro Gaete", "email": "pedro@gmail.com" }
+    return render(request, 'template/userInfoTemplate.html', data)
+
